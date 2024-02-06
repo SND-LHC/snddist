78d53a0d0490e87d0279a0686a37f197ac06a15a
Oliver Lantwin <oliver.lantwin@cern.ch>
2024-02-06 11:41:09 +0100

    RAVE: explicit module file, remove CLHEP workaround

--- rave.sh
+++ rave.sh
@@ -9,10 +9,10 @@ build-requires:
   - "GCC-Toolchain:(?!osx)"
 env:
   RAVEPATH: "$RAVE_ROOT"
+prepend_path:
+  LD_LIBRARY_PATH: "$RAVEPATH/lib"
 ---
 cd $SOURCEDIR
-
-cp -R "$CLHEP_ROOT/include/CLHEP" "$SOURCEDIR" # TODO fix CLHEP headers not being found
 
 ./bootstrap
 
@@ -34,9 +34,24 @@ MODULEDIR="$INSTALLROOT/etc/modulefiles"
 MODULEFILE="$MODULEDIR/$PKGNAME"
 mkdir -p "$MODULEDIR"
 
-alibuild-generate-module --lib > $MODULEFILE
 
 cat >> "$MODULEFILE" <<EoF
+#%Module1.0
+proc ModulesHelp { } {
+  global version
+  puts stderr "ALICE Modulefile for $PKGNAME $PKGVERSION-@@PKGREVISION@$PKGHASH@@"
+}
+set version $PKGVERSION-@@PKGREVISION@$PKGHASH@@
+module-whatis "ALICE Modulefile for $PKGNAME $PKGVERSION-@@PKGREVISION@$PKGHASH@@"
+# Dependencies
+if ![ is-loaded 'BASE/1.0' ] {
+ module load BASE/1.0
+}
+if ![ is-loaded "CLHEP/$CLHEP_VERSION-$CLHEP_REVISION" ] { module load "CLHEP/$CLHEP_VERSION-$CLHEP_REVISION"}
+
+set PKG_ROOT $::env(BASEDIR)/RAVE/\$version
+prepend-path LD_LIBRARY_PATH \$PKG_ROOT/lib
+
 # Our environment
 set RAVE_ROOT \$::env(BASEDIR)/$PKGNAME/\$version
 setenv RAVEPATH \$RAVE_ROOT
